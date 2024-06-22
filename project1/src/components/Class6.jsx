d8f89dd8cd8b88e6c819bfd5d1a51e3fe2dd5c34
Ganesh Dutt <gdutt2304@gmail.com>
2024-06-22 17:50:42 +0530

    Update btns

--- project1/src/components/Class6.jsx
+++ project1/src/components/Class6.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 export const Class6 = () => {
-    const [text, setText] = useState("Enter Your text here..")
+    const [text, setText] = useState("")
 
 
  const upperText = ()=>{
@@ -22,11 +22,14 @@ export const Class6 = () => {
   <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
 
-  <textarea class="form-control" onChange={(e)=>setText(e.target.value)} value={text} id="exampleFormControlTextarea1" rows="3"></textarea>
+  <textarea placeholder='Enter Your text here..' class="form-control" onChange={(e)=>setText(e.target.value)} value={text} id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
 
 <button className='btn btn-danger' onClick={upperText}>UpperText</button>
 <button className='btn btn-primary' onClick={LowerText}>LowerText</button>
+
+<h1>{text}</h1>
+<p> <span className='text-danger'>Word</span> : {text.split(" ").length}  AND <span className="text-danger">Char : </span>{text.length}</p>
     </div>
   )
 }
