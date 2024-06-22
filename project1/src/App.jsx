c7a8178ebf190ac0b2ba66daa600ae4935285463
Ganesh Dutt <gdutt2304@gmail.com>
2024-06-22 18:39:30 +0530

    Update file

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,15 +1,15 @@
-import { Class6 } from "./components/Class6"
-import { Yash } from "./components/Yash"
-import { Class5 } from "./components/class5"
-import 'bootstrap/dist/css/bootstrap.css'
+import { Class6 } from "./components/Class6";
+import { Yash } from "./components/Yash";
+import { Class5 } from "./components/class5";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-    return (
- <div>
-   {/* <Class5/> */}
-   <Class6/>
- </div>
-  )
+  return (
+    <div>
+      <Class5 />
+      <Class6 />
+    </div>
+  );
 }
 
-export default App
+export default App;
