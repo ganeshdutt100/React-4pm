3cb799e187ca01fe14d66957526957ef74be6b7b
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-18 17:25:03 +0530

    Class base components  syntax

---
+++ classbasecomponents/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import { Class1 } from "./Component/Class1";
+import Class2 from "./Component/Class2";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* <Class1 /> */}
+      <Class2 />
+    </>
+  );
+}
+
+export default App;
---
+++ classbasecomponents/src/Component/Class1.jsx
@@ -0,0 +1,14 @@
+import React, { Components } from "react";
+export class Class1 extends Components {
+  constructor() {}
+  render() {
+    return (
+      <>
+        <h1>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          minima.
+        </h1>
+      </>
+    );
+  }
+}
