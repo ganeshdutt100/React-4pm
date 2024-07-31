14870681b20c52d0040fe6fa389c55035e483048
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-31 17:37:19 +0530

    Redux

---
+++ redux/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./reduxFolder/counter/counterSlice";
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <button onClick={() => dispatch(increment())}>Add</button>
+      <input type="text" value={count} />
+      <button onClick={() => dispatch(decrement())}>Sub</button>
+    </>
+  );
+}
+
+export default App;
