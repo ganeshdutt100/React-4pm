dac0db7d139119ce654677715fae09765eb80687
Ganesh Dutt <gdutt2304@gmail.com>
2024-08-02 17:10:04 +0530

    Coding test for yash

--- Coding-Test/yash.js
+++ Coding-Test/yash.js
@@ -46,3 +46,59 @@ return (
 }
 
 export default App;
+
+
+// Interview Questions for Mid-level ReactJS Developers
+
+// Question 7
+// Will the below code throw any error when adding a new item to the list? If yes, what error will that be?
+
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: ["Item1", "Item2"] };
+  }
+  addItem() {
+    const newItem = "Item3";
+    this.state.list.push(newItem);
+    this.setState({ list: this.state.list });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addItem.bind(this)}>Add item</button>
+        <ul>
+          {this.state.list.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+	);
+  }
+}
+export default App;
+
+
+// Question 8
+// Build a simple drag-and-drop interface that allows users to reorder a list of items through drag-and-drop. Implement a visual cue to indicate the new position of the dragged item. Use React state, components and hooks to manage the list’s data and user interactions.
+
+// Question 9
+// What is wrong with the below code:
+
+import React, { useCallback, useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+ 
+  const increment = useCallback(() => {
+	setCount(count + 1);
+  }, []);
+   return (
+	<div>
+  	<button onClick={increment}>Increment</button>
+  	<p>Count: {count}</p>
+	</div>
+  );
+}
+ 
+export default App;
